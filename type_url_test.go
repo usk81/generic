85466e259462f5bec53de8779ade736607185351
Yusuke Komatsu <tienlen042@gmail.com>
2020-08-23 03:49:15 +0900

    fix tests

--- type_url_test.go
+++ type_url_test.go
@@ -1,5 +1,3 @@
-// +build go1.15
-
 package generic
 
 import (
@@ -450,11 +448,12 @@ var (
 			uri: URL{
 				ValidFlag: true,
 				url: &url.URL{
-					Scheme:   "http",
-					Host:     "www.google.com",
-					Path:     "/",
-					RawQuery: "q=go+language",
-					Fragment: "foo&bar",
+					Scheme:      "http",
+					Host:        "www.google.com",
+					Path:        "/",
+					RawQuery:    "q=go+language",
+					Fragment:    "foo&bar",
+					RawFragment: "foo%26bar",
 				},
 			},
 			hostname: "www.google.com",
